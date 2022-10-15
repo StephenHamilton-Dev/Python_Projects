6b141abcb8f00038a5f3125ada645018a700464b
Stephen Hamilton <61669798+StephenHamilton-Dev@users.noreply.github.com>
2022-10-15 12:15:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,35 +6,38 @@ game_board_spaces = {1 : "1", 2 : "2", 3 : "3",
                      7 : "7", 8 : "8", 9 : "9"}
 
 print("Welcome to Tic Tac Toe!\n"
-      "To quit the game, type the word quit and press enter.\n")
+      "\nTo quit the game, type the number 0 and press enter.\n")
 
 turn = 0
 playing = True
 
 while playing:
 
+    # draw the first game board
+    draw_board(game_board_spaces)
+
+    print("\n")
+
     # ask the player for input
-    player_input = input("Please enter in a number for spaces 1 - 9: ")
+    player_input = input("To continue the game, please enter in a number for spaces 1 - 9 on the board and press"
+                         " enter: ")
 
     # the player input, accessing a key in the dictionary, will be equal to each player turn
     game_board_spaces[int(player_input)] = player_turn(turn)
-
-    # draw the first game board
-    draw_board(game_board_spaces)
 
     # increment the turn by 1 every iteration of the loop
     turn += 1
 
     # if any combination of player win = true, end game and show the win, else, keep playing
     if player_win(game_board_spaces):
-        print("You have won the game")
+        print("You have won the game!")
         playing = False
     else:
         playing = True
 
-    # if the player input is the word quit, end the game, else, keep playing the game
-    if player_input == 'quit':
-        print("The game has ended")
+    # if the player input is the number zero, end the game, else, keep playing the game
+    if player_input == '0':
+        print("The game has ended.")
         playing = False
 
 # draw the final game board for the win
