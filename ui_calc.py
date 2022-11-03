6ffd69bb85a747767c2de66e7069a4db8e5fd21f
Stephen Hamilton <hamilton.wp.stephen@gmail.com>
2022-11-02 20:45:41 -0500

    new calculator code

--- ui_calc.py
+++ ui_calc.py
@@ -141,7 +141,7 @@ class Ui_MainWindow(object):
 
         QMetaObject.connectSlotsByName(MainWindow)
 
-    # commen
+    # comment
     def plus_minus(self):
         # grab what is on the screen
         screen = self.outputLabel.text()
